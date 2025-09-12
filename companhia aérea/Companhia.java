1d02c54b0be70d906994b4da82e041043ddf74ba
AmandaaViana <amandaviana.ahv@gmail.com>
2025-09-12 16:17:22 -0300

    Update Companhia.java

--- companhia aérea/Companhia.java
+++ companhia aérea/Companhia.java
@@ -1,7 +1,42 @@
 public class Companhia {
     
-
+    private String nome;
+    private String fone;
+    private int qtdeVoos; 
     private Voo [] vetVoos;
-
     
+    public Companhia(){
+        this.vetVoos = new Voo[10];
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public int getQtdeVoos() {
+        return qtdeVoos;
+    }
+    public void setQtdeVoos(int qtdeVoos) {
+        this.qtdeVoos = qtdeVoos;
+    }
+    
+    public Voo getVoos(int pos){
+        return vetVoos[pos];
+    }
+    public void setVoo(Voo v){
+        if(this.qtdeVoos < 10){
+            this.vetVoos[qtdeVoos] = v;
+            qtdeVoos++;
+        }
+    }
 }
